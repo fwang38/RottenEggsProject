04c556919e32204f1df01e41343845403db06fa8
fwang38 <jammie1212@hotmail.com>
2015-11-03 19:30:34 -0500

    hhhh

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var movies = require('./routes/movies');
+
 
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
+
+router.get('/movies', function(req, res, next) {
+	res.render('movies', { title: '12345' });
+});
+
 
 module.exports = router;
