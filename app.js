114173dc3df4947c2d000f596db5623a6c6f494c
boliu613 <liubo1678@gmail.com>
2015-11-03 19:09:18 -0500

    connect to AWS my SQL

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var vote = require('./routes/vote');
 
 var app = express();
 
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.get('/vote', vote.displayResponse);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Index' });
+});
+
+router.get('/homepage', function(req, res, next) {
+	res.render('homepage', {results: null});
 });
 
 module.exports = router;
