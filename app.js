03a4dde3652a5e3f3ecb87ca7763e2f27dc36024
mexu <mexu@seas.upenn.edu>
2015-11-15 14:45:52 -0500

    mmm

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use('/', routes);
 app.use('/users', users);
 
 app.get('/vote', vote.displayResponse);
+//app.get('/index',index.generateResponse);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
+function generateResponse(req, res) {
+	// The url to connect to the mongodb instance
+	var connection = mysql.createConnection({
+		  host     : 'mydb.c31kdvhm3rfj.us-west-2.rds.amazonaws.com',
+		  user     : 'boliu',
+		  password : 'liubo1678',
+		  database : 'RottenEggs'
+		});
+
+		connection.connect();
+
+		connection.query('SELECT genre from genre', function(err, rows, fields) {
+		  if (!err){
+			  console.log('The solution is: ', rows);
+			  var result=[];
+			  for (var i in rows) {
+			        result.push(rows[i].genre);
+			   }
+			    res.render('index',{results:null, genres:result});
+		  } 
+		  else
+		    console.log('Error while performing Query.');
+		});
+		connection.end();
+}
+function getmovies(req, db, callback){
+
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Index' });
+  generateResponse(req, res);
 });
 
 router.get('/homepage', function(req, res, next) {
+	
 	res.render('homepage', {results: null});
+});
+
+router.get('/comment', function(req, res, next) {
+	res.render('comment', {results: null});
 });
 
 module.exports = router;
