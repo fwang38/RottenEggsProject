0c0dad34d8dc51643d80f9db900cdc766b54c8fa
boliu613 <liubo1678@gmail.com>
2015-11-21 18:48:55 -0500

    vote

--- routes/vote.js
+++ routes/vote.js
@@ -1,61 +1,51 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 
-//function getResults(db, callback) {
-//	var cursor =db.collection('business').find({categories:{$in: ['Bars']},city:'Carnegie'});
-//	var results = [];
-//	cursor.each(function(err, doc) {
-//		if (doc != null) {
-//			//console.log(doc);
-//			results.push(doc.name);
-//		} else {
-//			callback(results);
-//		}
-//	});
-//};
-//
-//function generateResponse(req, res) {
-//	// The url to connect to the mongodb instance
-//	var url = 'mongodb://cis550student:cis550hw3@ds051933.mongolab.com:51933/cis550hw3';
-//	MongoClient.connect(url, function(err, db) {
-//		// If there is an error, log the error and render the error page 
-//		if(err != null) {
-//			console.log("Connection to server failed.");
-//			db.close();
-//			res.render('error', {
-//				message: "Connection to server failed.",
-//				error: err
-//			});
-//		}
-//		// If there is no error while connecting, proceed further
-//		else {
-//			console.log("Connected correctly to server.");
-//			getResults(db, function(results) {
-//				db.close();
-//				res.render('homepage.ejs', {results: results});
-//			});
-//		}
-//	});
-//}
-//
+function getResults(req, res, connection, callback) {
+	connection.query('SELECT * from personinfo limit 5', function(err, rows, fields) {
+		if (!err){
+			console.log('The solution is: ', rows);
+			callback(rows);			
+		}
+		else{
+			console.log('Error while performing Query.');
+			console.log('The solution is: ', rows);
+			res.render('error', {
+				message: "Error while performing Query.",
+				error: err
+			});			
+		}
+	});
+};
 
-exports.displayResponse = function(req, res){
-//	generateResponse(req, res);
-
+function generateResponse(req, res) {
 	var connection = mysql.createConnection({
-	  host     : 'mydb.c31kdvhm3rfj.us-west-2.rds.amazonaws.com',
-	  user     : 'boliu',
-	  password : 'liubo1678',
-	  database : 'RottenEggs'
+		host     : 'mydb.c31kdvhm3rfj.us-west-2.rds.amazonaws.com',
+		user     : 'boliu',
+		password : 'liubo1678',
+		database : 'RottenEggs'
 	});
 
-	connection.connect();
+	connection.connect(function(err){
+		if (!err){
+			console.log("Connected correctly to server.");			
+		}
+		else{
+			console.log("Connection to server failed.");
+			res.render('error', {
+				message: "Connection to server failed.",
+				error: err
+			});			
+		}
+	});	
 
-	connection.query('SELECT * from personinfo', function(err, rows, fields) {
-	  if (!err)
-	    console.log('The solution is: ', rows);
-	  else
-	    console.log('Error while performing Query.');
+	getResults(req, res, connection, function(results) {
+		//res.render('homepage.ejs', {results: results});
 	});
 
 	connection.end();
+}
+
+
+exports.displayResponse = function(req, res){
+	generateResponse(req, res);
 };
