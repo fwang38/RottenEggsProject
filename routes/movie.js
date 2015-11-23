708456faaa432c6251c783de9798d0eb6e20c486
boliu613 <liubo1678@gmail.com>
2015-11-23 00:34:46 -0500

    hello

--- routes/movie.js
+++ routes/movie.js
@@ -1,57 +1,54 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-var result=[];
-var resultperson=[];
-function keywordMovie(req, res){
-	var movie=req.query.movie;
-	console.log(req.query.movie);
-	console.log('aa');
-	//console.log(genre);
-		var connection = mysql.createConnection({
+//var result=[];
+//var resultperson=[];
+
+function doquery(q){
+	var q='SELECT * from movie m where m.title LIKE \'%'+ movie+'%\' limit 5';
+	var result=[];
+	
+	var connection = mysql.createConnection({
 		  host     : 'mydb.c31kdvhm3rfj.us-west-2.rds.amazonaws.com',
 		  user     : 'boliu',
 		  password : 'liubo1678',
 		  database : 'RottenEggs'
 		});
-		
-		var q='SELECT * from movie m where m.title LIKE \'%'+ movie+'%\' limit 25';
-		var q2='SELECT * from personinfo p where p.name LIKE \'%'+ movie+'%\' limit 25';
-		console.log(q);
-		console.log(q2);
-		connection.connect();
-		
-		connection.query(q , function(err, rows, fields) {
-		  if (!err){
-			  //console.log('The solution is: ', rows);
-			  
-			  for (var i in rows) {
-			        result.push(rows[i]);
-			   }
-			  //console.log(result);
-			  
-		  } 
-		  else
-		    console.log('Error while performing Query.');
-		});
-		//console.log(result);
-		connection.query(q2 , function(err, rows, fields) {
-			  if (!err){
-				  //console.log('The solution is: ', rows);
-				  
-				  for (var i in rows) {
-				        resultperson.push(rows[i]);
-				   }
-				  console.log(resultperson);
-			  } 
-			  else
-			    console.log('Error while performing Query.');
-			});
-		//console.log(result);
-		res.render('index',{results:null, resultsmovie:result, resultsperson:resultperson, user:req.user});
-		connection.end();
+	connection.connect();
+	
+	connection.query(q , function(err, rows, fields) {
+	  	if (!err){
+		  	for (var i in rows) {
+		        result.push(rows[i]);
+		   	}
+		  	console.log("result!!!!!");
+		  	console.log(result);
+		  	return result;
+		} 
+	  	else console.log('Error while performing Query.');
+	});
+	
+	connection.end();
+}
+
+function keywordMovie(req, res, callback){
+	
+	
+	var movie=req.query.movie;
+	console.log(req.query.movie);
+	
+	//console.log(doquery(q));
+	console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	var x = 	
+	var x1= 
+	callback(x,x1);
+	
 }
 //
 exports.displayResponse = function(req, res){
-	keywordMovie(req, res);
+	keywordMovie(req, res, function(resultsmovie, resultsperson){
+		console.log(resultsmovie);
+		console.log(resultsperson);
+		res.render('index',{results:null, resultsmovie:resultsmovie, resultsperson:resultsperson, user:req.user});
+	});
 };
