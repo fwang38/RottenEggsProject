d8c641e0937d7882da5bd044e8093ac685423489
fwang38 <jammie1212@hotmail.com>
2015-12-06 20:42:42 -0500

    fix commentid etc

--- routes/index.js
+++ routes/index.js
@@ -2,39 +2,54 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
+function generateResponse(req, res) {
+	var connection = mysql.createConnection({
+		host     : 'mydb.c31kdvhm3rfj.us-west-2.rds.amazonaws.com',
+		user     : 'boliu',
+		password : 'liubo1678',
+		database : 'RottenEggs',
+		multipleStatements: true
+	});
+
+	connection.connect(function(err){
+		if (!err){
+			console.log("Connected correctly to server.");			
+		}
+		else{
+			console.log("Connection to server failed.");
+			res.render('error', {
+				message: "Connection to server failed.",
+				error: err
+			});			
+		}
+	});
+	
+	var q1='select * from movie order by releasedate desc limit 12;';
+	var q2='select * from votes order by ts desc limit 10;';
+	var q3='select * from movie order by vote desc limit 10;';
+	var q=q1+q2+q3;
+	console.log(q);
+	connection.query(q , function(err, rows, fields) {
+	if (!err){
+		  console.log('The solution is: ');
+		  var result=rows[0];
+	      var recentvote=rows[1];
+	      var currentworst=rows[2];
+		  console.log(recentvote);
+		  console.log(currentworst);
+		  res.render('index',{bing: null, results:result, recentvote: recentvote, currentworst:currentworst, resultsperson:null, resultsmovie:null, user:req.user});
+	} 
+	else
+	    console.log('Error while performing Query.');
+	});
+	connection.end();
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var connection = mysql.createConnection({
-		  host     : 'mydb.c31kdvhm3rfj.us-west-2.rds.amazonaws.com',
-		  user     : 'boliu',
-		  password : 'liubo1678',
-		  database : 'RottenEggs'
-		});
-		var q='select * from movie order by releasedate desc limit 12';
-		console.log(q);
-		connection.connect();
-		connection.query(q , function(err, rows, fields) {
-		if (!err){
-			  console.log('The solution is: ', rows);
-			  var result=[];
-			  for (var i in rows) {
-			        result.push(rows[i]);
-			  }
-			  console.log(result);
-			  res.render('index',{bing: null, results:result, resultsperson:null, resultsmovie:null, user:req.user});
-		} 
-		else
-		    console.log('Error while performing Query.');
-		});
-		connection.end();
+	generateResponse(req, res);		
 });
 
-//, resultsperson:null
-//router.get('/homepage', function(req, res, next) {
-//	
-//	res.render('homepage', {results: null});
-//});
 
 router.get('/comment', function(req, res, next) {
 	res.render('comment', {results: null});
