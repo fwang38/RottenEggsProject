0233dfdfd4b9db534b2ed04ad1c72010b57a9b74
lj1010 <liujue@vagvlan536.0844.wlan.wireless-pennnet.upenn.edu>
2015-12-06 23:02:27 -0500

    homepage unvote

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,14 @@ function generateResponse(req, res) {
 	var q1='select * from movie order by releasedate desc limit 12;';
 	var q2='select * from votes order by ts desc limit 10;';
 	var q3='select * from movie order by vote desc limit 10;';
-	var q=q1+q2+q3;
+//	console.log(req.user !=null);
+	if (req.user != null) {
+		var q4='SELECT movie_id from votes v, movie m where v.userid =\''+ req.user.id +'\' and v.movieid=m.movie_id'
+	}
+	else {
+		var q4 = '';
+	}
+	var q=q1+q2+q3+q4;
 	console.log(q);
 	connection.query(q , function(err, rows, fields) {
 	if (!err){
@@ -35,9 +42,24 @@ function generateResponse(req, res) {
 		  var result=rows[0];
 	      var recentvote=rows[1];
 	      var currentworst=rows[2];
-		  console.log(recentvote);
-		  console.log(currentworst);
-		  res.render('index',{bing: null, results:result, recentvote: recentvote, currentworst:currentworst, resultsperson:null, resultsmovie:null, user:req.user});
+	      var hi=rows[3];
+	      var hasvoted = [];
+	      if (hi != null) {
+	    	  for (var i in hi){
+	    		  console.log('here-----');
+	    		  console.log(hi[i].movie_id);
+	    		  hasvoted.push(hi[i].movie_id);
+	    	  }
+	      }
+		  if (req.user!=null){
+			  console.log(hasvoted);
+		  }
+		  console.log(hasvoted.indexOf(205775));
+		  console.log(hasvoted.indexOf(11));
+		  console.log(11 in hasvoted);
+		  console.log(205775 in hasvoted);
+		  console.log('------'+hasvoted);
+		  res.render('index',{bing: null, results:result, hasvoted:hasvoted, recentvote: recentvote, currentworst:currentworst, resultsperson:null, resultsmovie:null, user:req.user});
 	} 
 	else
 	    console.log('Error while performing Query.');
